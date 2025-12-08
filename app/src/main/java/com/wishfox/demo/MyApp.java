464f2e1a5cc86230dfe9874a72f244c27b2660f3
范为广 <fanweiguang@vcglobal.com.cn>
2025-12-08 18:04:11 +0800

    优化悬浮球布局文件

--- app/src/main/java/com/wishfox/demo/MyApp.java
+++ app/src/main/java/com/wishfox/demo/MyApp.java
@@ -25,11 +25,11 @@ public class MyApp extends Application {
                         "1",
                         "billcomwishfoxdemoe"
                 )
-//                        .setBaseUrl("http://192.168.150.240:9113")
+                        .setBaseUrl("http://192.168.150.240:9113")
                         .setEnableLog(true)
                         .setWechatTest(true)
                         .setScreenOrientation(FoxSdkConfig.ORIENTATION_LANDSCAPE)
-                        .setFloatXScale(0.6f)
+                        .setFloatXScale(0.5f)
                         .setFloatXxOffset(100)
                         .build()
         );
