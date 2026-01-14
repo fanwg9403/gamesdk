470eed04589637273de27f2e96eef8506e6e164a
wjq <450023710@qq.com>
2026-01-14 11:49:04 +0800

    调整

--- app/src/main/java/com/wishfox/demo/MyApp.java
+++ app/src/main/java/com/wishfox/demo/MyApp.java
@@ -28,7 +28,7 @@ public class MyApp extends Application {
                         .setBaseUrl("http://192.168.150.240:9113")
                         .setEnableLog(true)
                         .setWechatTest(true)
-                        .setScreenOrientation(FoxSdkConfig.ORIENTATION_LANDSCAPE)
+                        .setScreenOrientation(FoxSdkConfig.ORIENTATION_PORTRAIT)
                         .setFloatXScale(0.5f)
                         .setFloatXxOffset(100)
                         .build()
